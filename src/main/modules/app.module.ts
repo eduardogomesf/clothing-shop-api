d8145c4bcd8e5aa430d078af37c2bdc6cae23c29
Eduardo Gomes <eduardogoomesf@gmail.com>
2023-01-01 12:05:01 -0300

    feat: add postgres integration with typeorm

---
+++ src/main/modules/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+    }),
+  ],
+})
+export class AppModule {}
