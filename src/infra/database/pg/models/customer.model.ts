c4fa5f434d0e6aabdee860543509c1af6c425546
Eduardo Gomes <eduardogoomesf@gmail.com>
2023-01-02 21:27:17 -0300

    feat: add customer migration and model

---
+++ src/infra/database/pg/models/customer.model.ts
@@ -0,0 +1,32 @@
+import { Customer } from "@/domain/entities/customer";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class CustomerModel implements Customer {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({
+    name: 'cellphone_number'
+  })
+  cellphoneNumber: string;
+
+  @Column('')
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn({
+    name: 'created_at'
+  })
+  createdAt: string
+
+  @UpdateDateColumn({
+    name: 'updated_at'
+  })
+  updatedAt: string
+}
