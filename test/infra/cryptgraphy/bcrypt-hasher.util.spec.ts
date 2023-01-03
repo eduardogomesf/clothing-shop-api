6a3f56636403dd8fe085d8d72569360511a94dc7
Eduardo Gomes <eduardogoomesf@gmail.com>
2023-01-03 08:42:31 -0300

    test: add Hasher with bcrypt

---
+++ test/infra/cryptgraphy/bcrypt-hasher.util.spec.ts
@@ -0,0 +1,40 @@
+import { Hasher } from "@/application/protocols/utils/cryptography/hasher.util"
+import bcrypt from 'bcrypt'
+import { BcryptHasher } from "../../../src/infra/utils/cryptography/bcrypt-hasher.util"
+
+jest.mock('bcrypt', () => {
+  return {
+    default: {
+      hash: () => {
+        return Promise.resolve('hashed-value' as never)
+      }
+    }
+  }
+})
+
+describe('Bcrypt Hasher', () => {
+  let bcryptHasher: Hasher
+
+  beforeEach(() => {
+    bcryptHasher = new BcryptHasher(8)
+  })
+
+  it('should return a hashed string', async () => {
+    const result = await bcryptHasher.hash('any-value')
+    expect(result).toBe('hashed-value')
+  })
+
+  it('should call Bcrypt.hash with correct values', async () => {
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await bcryptHasher.hash('any-value')
+    expect(hashSpy).toHaveBeenCalledWith('any-value', 8)
+  })
+
+  it('should pass along any error thrown by Bcrypt.hash', async () => {
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+      throw new Error('any-error')
+    })
+    const promise = bcryptHasher.hash('any-value')
+    await expect(promise).rejects.toThrowError(new Error('any-error'))
+  })
+})
