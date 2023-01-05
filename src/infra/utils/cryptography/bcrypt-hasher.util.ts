1a689afec71771c352eac0f623b1309d2d5ac391
Eduardo Gomes <eduardogoomesf@gmail.com>
2023-01-04 23:48:51 -0300

    fix: change bcrypt import

--- src/infra/utils/cryptography/bcrypt-hasher.util.ts
+++ src/infra/utils/cryptography/bcrypt-hasher.util.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt'
 import { Hasher } from "@/application/protocols/utils/cryptography/hasher.util";
 
 export class BcryptHasher implements Hasher {
