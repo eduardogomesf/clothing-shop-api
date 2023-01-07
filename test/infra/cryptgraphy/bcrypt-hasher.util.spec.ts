5163c16430eb760cfb8b5e67db7fdbb4fa1cafac
Eduardo Gomes <eduardogoomesf@gmail.com>
2023-01-07 13:34:05 -0300

    test: add hash comparer implementation with bcrypt

--- test/infra/cryptgraphy/bcrypt-hasher.util.spec.ts
+++ test/infra/cryptgraphy/bcrypt-hasher.util.spec.ts
@@ -1,4 +1,3 @@
-import { Hasher } from "@/application/protocols/utils/cryptography/hasher.util"
 import * as bcrypt from 'bcrypt'
 import { BcryptHasher } from "../../../src/infra/utils/cryptography/bcrypt-hasher.util"
 
@@ -6,33 +5,59 @@ jest.mock('bcrypt', () => {
   return {
     hash: () => {
       return Promise.resolve('hashed-value' as never)
+    },
+    compare: () => {
+      return Promise.resolve(true)
     }
   }
 })
 
 describe('Bcrypt Hasher', () => {
-  let bcryptHasher: Hasher
+  let bcryptHasher: BcryptHasher
 
   beforeEach(() => {
     bcryptHasher = new BcryptHasher(8)
   })
 
-  it('should return a hashed string', async () => {
-    const result = await bcryptHasher.hash('any-value')
-    expect(result).toBe('hashed-value')
-  })
-
-  it('should call Bcrypt.hash with correct values', async () => {
-    const hashSpy = jest.spyOn(bcrypt, 'hash')
-    await bcryptHasher.hash('any-value')
-    expect(hashSpy).toHaveBeenCalledWith('any-value', 8)
-  })
-
-  it('should pass along any error thrown by Bcrypt.hash', async () => {
-    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
-      throw new Error('any-error')
+  describe('hash', () => {
+    it('should return a hashed string', async () => {
+      const result = await bcryptHasher.hash('any-value')
+      expect(result).toBe('hashed-value')
     })
-    const promise = bcryptHasher.hash('any-value')
-    await expect(promise).rejects.toThrowError(new Error('any-error'))
+
+    it('should call Bcrypt.hash with correct values', async () => {
+      const hashSpy = jest.spyOn(bcrypt, 'hash')
+      await bcryptHasher.hash('any-value')
+      expect(hashSpy).toHaveBeenCalledWith('any-value', 8)
+    })
+
+    it('should pass along any error thrown by Bcrypt.hash', async () => {
+      jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+        throw new Error('any-error')
+      })
+      const promise = bcryptHasher.hash('any-value')
+      await expect(promise).rejects.toThrowError(new Error('any-error'))
+    })
+  })
+
+  describe('compare', () => {
+    it('should return the result of the comparison', async () => {
+      const result = await bcryptHasher.compare('hashed-value', 'value')
+      expect(result).toBe(true)
+    })
+
+    it('should call Bcrypt.compare with correct values', async () => {
+      const compareSpy = jest.spyOn(bcrypt, 'compare')
+      await bcryptHasher.compare('hashed-value', 'value')
+      expect(compareSpy).toBeCalledWith('value', 'hashed-value')
+    })
+
+    it('should pass along any error thrown by Bcrypt.compare', async () => {
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => {
+        throw new Error('any-error')
+      })
+      const promise = bcryptHasher.compare('hashed-value', 'value')
+      await expect(promise).rejects.toThrowError(new Error('any-error'))
+    })
   })
 })
