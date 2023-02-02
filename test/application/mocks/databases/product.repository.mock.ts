6a78508c2c1f7d9b253461aaaea7039ac269d379
Eduardo Gomes <eduardogoomesf@gmail.com>
2023-02-02 19:49:22 -0300

    test: add get products use case implementation

---
+++ test/application/mocks/databases/product.repository.mock.ts
@@ -0,0 +1,21 @@
+import { GetAllProductsWithFiltersRepository } from '@/application/protocols/database/repositories/customer-address/get-all-products-with-filters.repository'
+import { Product } from '../../../../src/domain/entities/product'
+
+export class ProductRepositoryStub implements GetAllProductsWithFiltersRepository {
+  async getWithFilters (): Promise<Product[]> {
+    return Promise.resolve([{
+      id: 'any-product-id',
+      name: 'Any Product',
+      categorySubCategoryId: 'any-category-subcategory-id',
+      variations: [{
+        id: 'any-variation-id',
+        price: 20,
+        stock: 20,
+        productId: 'any-product-id',
+        color: 'Black',
+        size: 'G',
+        imageUrl: 'www.google.com.br'
+      }]
+    }])
+  }
+}
---
+++ test/application/use-cases/product/imp-get-products.use-case.spec.ts
@@ -0,0 +1,56 @@
+import { GetAllProductsWithFiltersRepository } from '../../../../src/application/protocols/database/repositories/customer-address/get-all-products-with-filters.repository'
+import { ImpGetProductsUseCase } from '../../../../src/application/use-cases/product/imp-get-products.use-case'
+import { ProductRepositoryStub } from '../../mocks/databases/product.repository.mock'
+
+describe('GetProductsUseCase', () => {
+  let sut: ImpGetProductsUseCase
+  let getAllProductsWithFiltersRepository: GetAllProductsWithFiltersRepository
+
+  beforeEach(() => {
+    getAllProductsWithFiltersRepository = new ProductRepositoryStub()
+    sut = new ImpGetProductsUseCase(getAllProductsWithFiltersRepository)
+  })
+
+  it('should get a list of products with its variations', async () => {
+    const getAllSpy = jest.spyOn(getAllProductsWithFiltersRepository, 'getWithFilters')
+
+    const result = await sut.get()
+
+    expect(result).toBeDefined()
+    expect(getAllSpy).toHaveBeenCalledTimes(1)
+    expect(result).toHaveLength(1)
+    expect(result[0]).toHaveProperty('id')
+    expect(result[0]).toHaveProperty('name')
+    expect(result[0]).toHaveProperty('categorySubCategoryId')
+    expect(result[0].variations).toHaveLength(1)
+    expect(result[0].variations[0]).toHaveProperty('id')
+    expect(result[0].variations[0]).toHaveProperty('price')
+    expect(result[0].variations[0]).toHaveProperty('stock')
+    expect(result[0].variations[0]).toHaveProperty('productId')
+    expect(result[0].variations[0]).toHaveProperty('color')
+    expect(result[0].variations[0]).toHaveProperty('size')
+    expect(result[0].variations[0]).toHaveProperty('imageUrl')
+  })
+
+  it('should get a empty list if repository returns null', async () => {
+    getAllProductsWithFiltersRepository.getWithFilters = jest.fn().mockResolvedValueOnce(Promise.resolve(null))
+    const result = await sut.get()
+
+    expect(result).toEqual([])
+    expect(result).toHaveLength(0)
+  })
+
+  it('should get a empty list if repository returns an empty array', async () => {
+    getAllProductsWithFiltersRepository.getWithFilters = jest.fn().mockResolvedValueOnce(Promise.resolve([]))
+    const result = await sut.get()
+
+    expect(result).toEqual([])
+    expect(result).toHaveLength(0)
+  })
+
+  it('should pass along any exception thrown by GetAllProductsWithFiltersRepository.getWithFilters', async () => {
+    getAllProductsWithFiltersRepository.getWithFilters = jest.fn().mockImplementation(() => { throw new Error('any_error') })
+    const promise = sut.get()
+    expect(promise).rejects.toThrowError(new Error('any_error'))
+  })
+})
