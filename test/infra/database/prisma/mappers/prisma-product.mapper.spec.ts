fd74c1bbc2d5f7138651142981e9cda3ac5b7d01
Eduardo Gomes <eduardogoomesf@gmail.com>
2023-02-25 00:02:12 -0300

    test: add GetProductByIdWithVariationsRepository implementation

--- test/infra/database/prisma/mappers/prisma-product.mapper.spec.ts
+++ test/infra/database/prisma/mappers/prisma-product.mapper.spec.ts
@@ -7,48 +7,119 @@ describe('PrismaProductMapper', () => {
     sut = new PrismaProductMapper()
   })
 
-  it('should return a list of mapped products with its variations', () => {
-    const result = sut.mapProductsWithVariations([
-      {
-        id: 'any-id',
-        name: 'any-name',
-        description: 'any-description',
-        categorySubcategoryId: 'any-category-subcategory-id',
+  describe('mapProductsWithVariations()', () => {
+    it('should return a list of mapped products with its variations', () => {
+      const result = sut.mapProductsWithVariations([
+        {
+          id: 'any-id',
+          name: 'any-name',
+          description: 'any-description',
+          categorySubcategoryId: 'any-category-subcategory-id',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          variations: [{
+            id: 'any-variation-id',
+            color: 'any-color',
+            createdAt: new Date(),
+            imageUrl: 'any-image-url',
+            price: 50,
+            productId: 'any-product-id',
+            size: 'any-size',
+            stock: 50,
+            updatedAt: new Date()
+          }]
+        }
+      ])
+      expect(result).toEqual([
+        {
+          id: 'any-id',
+          name: 'any-name',
+          description: 'any-description',
+          categorySubcategoryId: 'any-category-subcategory-id',
+          variations: [{
+            id: 'any-variation-id',
+            color: 'any-color',
+            imageUrl: 'any-image-url',
+            price: 50,
+            size: 'any-size',
+            stock: 50
+          }]
+        }
+      ])
+    })
+
+    it('should return an empty list of products', () => {
+      const result = sut.mapProductsWithVariations([])
+      expect(result).toEqual([])
+    })
+  })
+
+  describe('mapProductDetailsWithVariations()', () => {
+    it('should return a product details with its variation', () => {
+      const result = sut.mapProductDetailsWithVariations({
+        id: 'any-product-id',
+        name: 'any-product-name',
+        description: 'any-description-description',
         createdAt: new Date(),
         updatedAt: new Date(),
+        categorySubcategory: {
+          id: 'any-category-subcategory-id',
+          subcategoryId: 'any-subcategory-id',
+          categoryId: 'any-category-id',
+          category: {
+            id: 'any-category-id',
+            name: 'any-category-name',
+            description: 'any-category-description',
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          subcategory: {
+            id: 'any-subcategory-id',
+            name: 'any-subcategory-name',
+            description: 'any-subcategory-description',
+            createdAt: new Date(),
+            updatedAt: new Date()
+          }
+        },
+        categorySubcategoryId: 'any-category-subcategory-id',
         variations: [{
           id: 'any-variation-id',
           color: 'any-color',
           createdAt: new Date(),
+          updatedAt: new Date(),
           imageUrl: 'any-image-url',
-          price: 50,
+          price: 0,
           productId: 'any-product-id',
           size: 'any-size',
-          stock: 50,
-          updatedAt: new Date()
+          stock: 10
         }]
-      }
-    ])
-    expect(result).toEqual([
-      {
-        id: 'any-id',
-        name: 'any-name',
-        description: 'any-description',
-        categorySubcategoryId: 'any-category-subcategory-id',
+      })
+      expect(result).toEqual({
+        id: 'any-product-id',
+        name: 'any-product-name',
+        description: 'any-description-description',
+        category: {
+          id: 'any-category-id',
+          name: 'any-category-name',
+          subcategory: {
+            id: 'any-subcategory-id',
+            name: 'any-subcategory-name'
+          }
+        },
         variations: [{
           id: 'any-variation-id',
           color: 'any-color',
           imageUrl: 'any-image-url',
-          price: 50,
+          price: 0,
           size: 'any-size',
-          stock: 50
+          stock: 10
         }]
-      }
-    ])
-  })
+      })
+    })
 
-  it('should return an empty list of products', () => {
-    const result = sut.mapProductsWithVariations([])
-    expect(result).toEqual([])
+    it('should return null if no product is found', () => {
+      const result = sut.mapProductDetailsWithVariations(null as any)
+      expect(result).toBeNull()
+    })
   })
 })
